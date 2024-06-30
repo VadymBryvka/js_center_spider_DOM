86242d77a6e90c77248c55784d3d0e022a272fbe
Vadym Bryvka <vadym.bryvkaa@gmail.com>
2024-06-30 16:09:31 -0500

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const spider = document.getElementsByClassName("spider")[0];
+const copyOfSpider = [
+    ...document.getElementsByClassName("spider")
+];
+const copyOfWall = [
+    ...document.getElementsByClassName("wall")
+];
+spider.style.top = (copyOfWall[0].clientHeight - copyOfSpider[0].clientHeight) / 2 + "px";
+spider.style.left = (copyOfWall[0].clientWidth - copyOfSpider[0].clientWidth) / 2 + "px"; // write code here
+
+//# sourceMappingURL=index.f75de5e1.js.map
